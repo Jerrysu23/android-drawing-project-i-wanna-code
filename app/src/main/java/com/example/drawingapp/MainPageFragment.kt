aa6fbc094a6a5c53d41d65a8fc90e27ccb5c88c7
justimus808 <jbejenaru20@seaburyhall.org>
2024-02-15 19:55:22 -0700

    created a new item in the view model when the new drawing button is clicked.

--- app/src/main/java/com/example/drawingapp/MainPageFragment.kt
+++ app/src/main/java/com/example/drawingapp/MainPageFragment.kt
@@ -1,18 +1,21 @@
 package com.example.drawingapp
 
+import android.graphics.Bitmap
+import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 
 import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import androidx.navigation.fragment.findNavController
 import com.example.drawingapp.databinding.FragmentMainPageBinding
 
 class MainPageFragment : Fragment() {
-
+    private val myViewModel : DrawingViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,6 +23,7 @@ class MainPageFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding = FragmentMainPageBinding.inflate(layoutInflater, container, false)
         binding.newDrawingButton.setOnClickListener{
+            myViewModel.newItem(Bitmap.createBitmap(800, 800, Bitmap.Config.ARGB_8888), Paint(), pensize = 5)
             findNavController().navigate(R.id.makeNewDrawing)
         }
         return binding.root
