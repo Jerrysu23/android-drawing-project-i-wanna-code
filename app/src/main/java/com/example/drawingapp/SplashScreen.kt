abd8a3b1f5b60c7760d2829a456f57a5f125abcd
Jerrysu23 <sujunlin0000@gmail.com>
2024-02-16 17:15:06 -0700

    Finished SplashScreen and everything about it

--- app/src/main/java/com/example/drawingapp/SplashScreen.kt
+++ app/src/main/java/com/example/drawingapp/SplashScreen.kt
@@ -2,11 +2,13 @@ package com.example.drawingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : AppCompatActivity() {
+class SlashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
