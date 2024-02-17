ede2a07b474d83179fa03ccc1c0b935ebc805da4
Sam Hill <soshjam@soshjam.com>
2024-02-16 19:48:32 -0700

    Change inflater to be more consistent

--- app/src/main/java/com/example/drawingapp/MainPageFragment.kt
+++ app/src/main/java/com/example/drawingapp/MainPageFragment.kt
@@ -1,32 +1,39 @@
 package com.example.drawingapp
 
-import android.graphics.Bitmap
-import android.graphics.Paint
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.activityViewModels
-
-import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import androidx.navigation.fragment.findNavController
-import com.example.drawingapp.databinding.FragmentMainPageBinding
 
 class MainPageFragment : Fragment() {
-    private val myViewModel : DrawingViewModel by activityViewModels()
+    private val viewModel : DrawingViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = FragmentMainPageBinding.inflate(layoutInflater, container, false)
-        binding.newDrawingButton.setOnClickListener{
-            myViewModel.addBitmap()
+//        val binding = FragmentMainPageBinding.inflate(layoutInflater, container, false)
+//        binding.newDrawingButton.setOnClickListener{
+//            myViewModel.addBitmap()
+//            findNavController().navigate(R.id.makeNewDrawing)
+//        }
+//        return binding.root
+
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_main_page, container, false)
+
+        // Add listener for the drawing button
+        view.findViewById<Button>(R.id.newDrawingButton).setOnClickListener {
+            viewModel.addBitmap()
             findNavController().navigate(R.id.makeNewDrawing)
         }
-        return binding.root
+
+        return view
     }
 
 
