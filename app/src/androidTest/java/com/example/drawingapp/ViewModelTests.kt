fd6293afbc8c93696f2d8beb78f636ad193d9c2e
Sam Hill <soshjam@soshjam.com>
2024-03-22 20:50:09 -0600

    Tests for adding drawings and updating drawings

--- app/src/androidTest/java/com/example/drawingapp/ViewModelTests.kt
+++ app/src/androidTest/java/com/example/drawingapp/ViewModelTests.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.util.Log
+import androidx.core.graphics.get
 import androidx.core.graphics.set
 import androidx.lifecycle.Observer
 import org.junit.Test
@@ -101,5 +102,81 @@ class ViewModelTests {
                 assertNotSame(before, vm.penColor.value)
             }}
         }
+    }
+
+    // Ensure we can add drawings
+    @Test
+    fun testAddDrawings() {
+        val vm = DrawingViewModel(repository)
+        runBlocking {
+            val lifecycleOwner = TestLifecycleOwner()
+            lifecycleOwner.run{ withContext(Dispatchers.Main) {
+                /* Begin Test */
+
+                // Add two drawings and get their IDs
+                val firstId: Long = vm.addDrawing()
+                val secondId: Long = vm.addDrawing()
+
+                // Ensure the IDs are separate
+                assertFalse(firstId == secondId)
+
+                /* End Test */
+            }}
+        }
+    }
+
+    // Ensure we can update drawings
+    // TODO: Find out why this test is failing but works in production.
+    @Test
+    fun testUpdateDrawings() {
+        val vm = DrawingViewModel(repository)
+        runBlocking {
+            val lifecycleOwner = TestLifecycleOwner()
+            lifecycleOwner.run{ withContext(Dispatchers.Main) {
+                /* Begin Test */
+
+                // Add a drawing
+                val firstId = vm.addDrawing()
+
+                // Add a red pixel to the top left and update it
+                vm.getCurrentDrawing().let{
+                    it[0, 0] = Color.RED
+                    Log.d("TestDrawing 1a", "${checkImageEmpty(it)}")
+                    vm.updateDrawing(it, firstId)
+                }
+
+                // Add another drawing
+                val secondId = vm.addDrawing()
+
+                // Add a blue pixel to the top right and update it
+                vm.getCurrentDrawing().let{
+                    it[0, 0] = Color.BLUE
+                    Log.d("TestDrawing 2a", "${checkImageEmpty(it)}")
+                    vm.updateDrawing(it, secondId)
+                }
+
+                Log.d("TestDrawing 1b", "${checkImageEmpty(vm.getDrawingById(firstId))}")
+                Log.d("TestDrawing 2b", "${checkImageEmpty(vm.getDrawingById(secondId))}")
+
+                // Make sure it updated our second drawing
+                assertEquals(Color.BLUE, vm.getCurrentDrawing()[0, 0])
+
+                // Make sure it updated our first drawing
+                assertEquals(Color.RED, vm.getDrawingById(firstId)[0, 0])
+
+                /* End Test */
+            }}
+        }
+    }
+
+    private fun checkImageEmpty(bitmap: Bitmap) : Boolean {
+        for (x in 0..<bitmap.width) {
+            for (y in 0..<bitmap.height) {
+                if (bitmap[x, y] != 0) {
+                    return false
+                }
+            }
+        }
+        return true
     }
 }
