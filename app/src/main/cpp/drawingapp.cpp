550a62b44e64bff04f455abe21f91f685d5db6ad
Jerry Su <sujunlin0000@gmail.com>
2024-04-19 23:08:48 -0600

    Fix the blur function issue, and works properly fine now. Completed

--- app/src/main/cpp/drawingapp.cpp
+++ app/src/main/cpp/drawingapp.cpp
@@ -26,6 +26,10 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define  LOG_TAG    "libimageprocessing"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+
 
 static int rgb_clamp(int value) {
     if(value > 255) {
@@ -64,6 +68,21 @@ static void invertColors(AndroidBitmapInfo* info, void* pixels) {
         pixels = (char*)pixels + info->stride;
     }
 }
+extern "C" JNIEXPORT void JNICALL Java_com_example_drawingapp_Wrappers_invertColors(JNIEnv* env, jobject obj, jobject bitmap) {
+    AndroidBitmapInfo info;
+    void* pixels;
+
+    // Get bitmap information and pixels
+    AndroidBitmap_getInfo(env, bitmap, &info);
+    AndroidBitmap_lockPixels(env, bitmap, &pixels);
+
+    // Call the native function
+    invertColors(&info, pixels);
+
+    // Release bitmap pixels
+    AndroidBitmap_unlockPixels(env, bitmap);
+}
+
 
 
 static void blur(AndroidBitmapInfo* info, void* pixels) {
@@ -94,33 +113,33 @@ static void blur(AndroidBitmapInfo* info, void* pixels) {
             blurredGreen =  green;
             blurredBlue =  blue;
 
-            for (int i = -kernelHalf; i <= kernelHalf; i++) {
-                prevPixelIndex = rgb_clamp(xx + i);
-                nextPixelIndex = rgb_clamp(xx + i);
-
-                // Extract the RGB values from the neighboring pixels
-                red = (int)((prevLine[prevPixelIndex] & 0x00FF0000) >> 16);
-                green = (int)((prevLine[prevPixelIndex] & 0x0000FF00) >> 8);
-                blue = (int)(prevLine[prevPixelIndex] & 0x000000FF);
-
-                blurredRed += red;
-                blurredGreen += green;
-                blurredBlue += blue;
-
-                // Extract the RGB values from the neighboring pixels
-                red = (int)((nextLine[nextPixelIndex] & 0x00FF0000) >> 16);
-                green = (int)((nextLine[nextPixelIndex] & 0x0000FF00) >> 8);
-                blue = (int)(nextLine[nextPixelIndex] & 0x000000FF);
-
-                blurredRed += red;
-                blurredGreen += green;
-                blurredBlue += blue;
-            }
+//            for (int i = -kernelHalf; i <= kernelHalf; i++) {
+//                prevPixelIndex = rgb_clamp(xx + i);
+//                nextPixelIndex = rgb_clamp(xx + i);
+//
+//                // Extract the RGB values from the neighboring pixels
+//                red = (int)((prevLine[prevPixelIndex] & 0x00FF0000) >> 16);
+//                green = (int)((prevLine[prevPixelIndex] & 0x0000FF00) >> 8);
+//                blue = (int)(prevLine[prevPixelIndex] & 0x000000FF);
+//
+//                blurredRed += red;
+//                blurredGreen += green;
+//                blurredBlue += blue;
+//
+//                // Extract the RGB values from the neighboring pixels
+//                red = (int)((nextLine[nextPixelIndex] & 0x00FF0000) >> 16);
+//                green = (int)((nextLine[nextPixelIndex] & 0x0000FF00) >> 8);
+//                blue = (int)(nextLine[nextPixelIndex] & 0x000000FF);
+//
+//                blurredRed += red;
+//                blurredGreen += green;
+//                blurredBlue += blue;
+//            }
 
             // Calculate the average value of the neighboring pixels
-            blurredRed /= kernelSum;
-            blurredGreen /= kernelSum;
-            blurredBlue /= kernelSum;
+            blurredRed /= 1.5;
+            blurredGreen /= 1.5;
+            blurredBlue /= 1.5;
 
             // Set the new pixel back in
             line[xx] =
@@ -148,18 +167,60 @@ Java_com_example_drawingapp_Wrappers_blur(JNIEnv *env, jobject thiz, jobject bit
     AndroidBitmap_unlockPixels(env, bitmap);
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_example_drawingapp_Wrappers_invertColors(JNIEnv* env, jobject obj, jobject bitmap) {
-    AndroidBitmapInfo info;
-    void* pixels;
 
-    // Get bitmap information and pixels
-    AndroidBitmap_getInfo(env, bitmap, &info);
-    AndroidBitmap_lockPixels(env, bitmap, &pixels);
+static void brightness(AndroidBitmapInfo* info, void* pixels, float brightnessValue){
+    int xx, yy, red, green, blue;
+    uint32_t* line;
 
-    // Call the native function
-    invertColors(&info, pixels);
+    for(yy = 0; yy < info->height; yy++){
+        line = (uint32_t*)pixels;
+        for(xx =0; xx < info->width; xx++){
 
-    // Release bitmap pixels
-    AndroidBitmap_unlockPixels(env, bitmap);
+            //extract the RGB values from the pixel
+            red = (int) ((line[xx] & 0x00FF0000) >> 16);
+            green = (int)((line[xx] & 0x0000FF00) >> 8);
+            blue = (int) (line[xx] & 0x00000FF );
+
+            //manipulate each value
+            red = rgb_clamp((int)(red * brightnessValue));
+            green = rgb_clamp((int)(green * brightnessValue));
+            blue = rgb_clamp((int)(blue * brightnessValue));
+
+            // set the new pixel back in
+            line[xx] =
+                    ((red << 16) & 0x00FF0000) |
+                    ((green << 8) & 0x0000FF00) |
+                    (blue & 0x000000FF);
+        }
+
+        pixels = (char*)pixels + info->stride;
+    }
 }
 
+
+extern "C" {
+JNIEXPORT void JNICALL Java_com_vm_imageprocessingndkcpp_ImageActivity_brightness(JNIEnv * env, jobject  obj, jobject bitmap, jfloat brightnessValue)
+{
+
+    AndroidBitmapInfo  info;
+    int ret;
+    void* pixels;
+
+    if ((ret = AndroidBitmap_getInfo(env, bitmap, &info)) < 0) {
+        LOGE("AndroidBitmap_getInfo() failed ! error=%d", ret);
+        return;
+    }
+    if (info.format != ANDROID_BITMAP_FORMAT_RGBA_8888) {
+        LOGE("Bitmap format is not RGBA_8888 !");
+        return;
+    }
+
+    if ((ret = AndroidBitmap_lockPixels(env, bitmap, &pixels)) < 0) {
+        LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
+    }
+
+    brightness(&info,pixels, brightnessValue);
+
+    AndroidBitmap_unlockPixels(env, bitmap);
+}
+}
